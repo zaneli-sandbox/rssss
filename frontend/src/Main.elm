9a190b655906e2961cf91151cf1f629f2c41d587
zaneli <shun.otani@gmail.com>
2019-01-09 17:08:00 +0900

    send request to backend and view rss items

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -1,20 +1,43 @@
-module Main exposing (..)
+module Main exposing (Model, Msg(..), init, main, update, view)
 
 import Browser
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import Html exposing (Html, a, button, div, h1, img, input, span, text)
+import Html.Attributes exposing (href, placeholder, src, title, value)
+import Html.Events exposing (onClick, onInput)
+import Http
+import Json.Decode as Decode
+
 
 
 ---- MODEL ----
 
 
 type alias Model =
-    {}
+    { url : String
+    , items : List Item
+    }
+
+
+type alias Item =
+    { title : String
+    , pubDate : Maybe String
+    , link : String
+    , description : String
+    }
+
+
+itemDecoder : Decode.Decoder Item
+itemDecoder =
+    Decode.map4 Item
+        (Decode.field "title" Decode.string)
+        (Decode.maybe (Decode.field "pub_date" Decode.string))
+        (Decode.field "link" Decode.string)
+        (Decode.field "description" Decode.string)
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( { url = "", items = [] }, Cmd.none )
 
 
 
@@ -23,11 +46,35 @@ init =
 
 type Msg
     = NoOp
+    | InputURL String
+    | GetRSS
+    | GotRSS (Result Http.Error (List Item))
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+        InputURL url ->
+            ( { model | url = url }, Cmd.none )
+
+        GetRSS ->
+            ( model, Http.get { url = buildUrl model, expect = Http.expectJson GotRSS (Decode.list itemDecoder) } )
+
+        GotRSS result ->
+            case result of
+                Ok items ->
+                    ( { model | items = items }, Cmd.none )
+
+                Err e ->
+                    ( model, Cmd.none )
+
+
+buildUrl : Model -> String
+buildUrl model =
+    "http://localhost:8080/feed?url=" ++ model.url
 
 
 
@@ -37,8 +84,18 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+        [ input [ placeholder "input RSS URL", value model.url, onInput InputURL ] []
+        , button [ onClick GetRSS ] [ text "view RSS" ]
+        , div []
+            (List.map
+                (\item ->
+                    div []
+                        [ span [] [ text (Maybe.withDefault "-" item.pubDate) ]
+                        , span [ title item.description ] [ a [ href item.link ] [ text item.title ] ]
+                        ]
+                )
+                model.items
+            )
         ]
 
 

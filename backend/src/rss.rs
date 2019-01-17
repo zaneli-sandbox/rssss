6a454240576e8ab818b8795cd31baaeb0cb217e8
zaneli <shun.otani@gmail.com>
2019-01-17 14:08:25 +0900

    RssParser as trait

--- backend/src/rss.rs
+++ backend/src/rss.rs
@@ -4,9 +4,10 @@ use error::ErrorKind::InvalidRssError;
 use scraper::Html;
 use xml::attribute::OwnedAttribute;
 use xml::name::OwnedName;
+use xml::namespace::Namespace;
 use xml::reader::{EventReader, XmlEvent};
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize, Clone)]
 pub struct Rss {
     title: String,
     description: String,
@@ -15,82 +16,140 @@ pub struct Rss {
 }
 
 pub fn parse_rss(buf: bytes::Bytes) -> Result<Vec<Rss>, Error> {
-    let parser = EventReader::new(buf.into_buf());
-    let mut tag = (Option::default(), String::new());
-    let mut title = String::new();
-    let mut link = String::new();
-    let mut description = String::new();
-    let mut pub_date = Option::default();
-    let mut rs: Vec<Rss> = Vec::new();
+    parse(&buf, &mut RssV20::new())
+}
+
+fn parse(buf: &bytes::Bytes, parser: &mut RssParser) -> Result<Vec<Rss>, Error> {
+    let reader = EventReader::new(buf.into_buf());
 
     let mut root = true;
-    for elem in parser {
+    for elem in reader {
         match elem? {
             XmlEvent::StartElement {
-                name, attributes, ..
+                name,
+                attributes,
+                namespace,
             } => {
+                parser.parse_start_element(name, attributes, namespace);
                 if root {
-                    verify_rss(&name, &attributes)?;
+                    parser.verify_rss()?;
                     root = false;
                 }
-                tag = (name.namespace, name.local_name);
             }
             XmlEvent::Characters(data) | XmlEvent::CData(data) => {
-                let (namespace, local_name) = &tag;
-                match (namespace.as_ref().map(|n| n.as_ref()), local_name.as_ref()) {
-                    (_, "title") => title = data,
-                    (_, "link") => link = data,
-                    (_, "description") => description = pick_texts(data),
-                    (Some("http://purl.org/rss/1.0/modules/content/"), "encoded") => {
-                        if description.is_empty() {
-                            description = pick_texts(data)
-                        }
-                    }
-                    (_, "pubDate") => pub_date = Some(data),
-                    _ => (),
-                }
+                parser.parse_content(data);
             }
             XmlEvent::EndElement { name } => {
-                if name.to_string() == "item" {
-                    rs.push(Rss {
-                        title: title.clone(),
-                        link: link.clone(),
-                        description: description.clone(),
-                        pub_date: pub_date.clone(),
-                    });
-                    title = String::new();
-                    link = String::new();
-                    description = String::new();
-                    pub_date = Option::default();
-                }
-                tag = (Option::default(), String::new());
+                parser.parse_end_element(name);
             }
             _ => (),
         };
     }
-    Ok(rs)
+    Ok(parser.get_results())
 }
 
-fn verify_rss(name: &OwnedName, attributes: &Vec<OwnedAttribute>) -> Result<(), Error> {
-    if name.local_name != "rss" {
-        return Err(Error::from(InvalidRssError(format!(
-            "invalid root element name: {}",
-            name.local_name
-        ))));
+trait RssParser {
+    fn parse_start_element(&mut self, OwnedName, Vec<OwnedAttribute>, Namespace);
+    fn parse_content(&mut self, String);
+    fn parse_end_element(&mut self, OwnedName);
+    fn verify_rss(&self) -> Result<(), Error>;
+    fn get_results(&self) -> Vec<Rss>;
+}
+
+struct RssV20 {
+    results: Vec<Rss>,
+    element: (OwnedName, Vec<OwnedAttribute>, Namespace),
+    title: String,
+    link: String,
+    description: String,
+    pub_date: Option<String>,
+}
+
+impl RssV20 {
+    fn new() -> RssV20 {
+        RssV20 {
+            results: Vec::new(),
+            element: (
+                OwnedName::local(String::new()),
+                Vec::default(),
+                Namespace::empty(),
+            ),
+            title: String::new(),
+            link: String::new(),
+            description: String::new(),
+            pub_date: Option::default(),
+        }
     }
-    let version = attributes
-        .iter()
-        .find(|a| a.name.to_string() == "version")
-        .map(|a| a.value.as_ref());
-    match version {
-        Some("2.0") => Ok(()),
-        Some(version) => Err(Error::from(InvalidRssError(format!(
-            "unsupported rss version: {}",
-            version
-        )))),
-        None => Err(Error::from(InvalidRssError(
-            "missing version attribute".to_string(),
-        ))),
+}
+
+impl RssParser for RssV20 {
+    fn parse_start_element(&mut self, name: OwnedName, attrs: Vec<OwnedAttribute>, ns: Namespace) {
+        self.element = (name, attrs, ns)
+    }
+    fn parse_content(&mut self, data: String) {
+        let (name, _, _) = &self.element;
+        match (
+            name.namespace.as_ref().map(|n| n.as_ref()),
+            name.local_name.as_ref(),
+        ) {
+            (_, "title") => self.title = data,
+            (_, "link") => self.link = data,
+            (_, "description") => self.description = pick_texts(data),
+            (Some("http://purl.org/rss/1.0/modules/content/"), "encoded") => {
+                if self.description.is_empty() {
+                    self.description = pick_texts(data)
+                }
+            }
+            (_, "pubDate") => self.pub_date = Some(data),
+            _ => (),
+        }
+    }
+    fn parse_end_element(&mut self, name: OwnedName) {
+        if name.to_string() == "item" {
+            let rss = Rss {
+                title: self.title.clone(),
+                link: self.link.clone(),
+                description: self.description.clone(),
+                pub_date: self.pub_date.clone(),
+            };
+            self.results.push(rss);
+
+            self.title = String::new();
+            self.link = String::new();
+            self.description = String::new();
+            self.pub_date = Option::default();
+        }
+        self.element = (
+            OwnedName::local(String::new()),
+            Vec::default(),
+            Namespace::empty(),
+        );
+    }
+    fn verify_rss(&self) -> Result<(), Error> {
+        let (name, attrs, _) = &self.element;
+        if name.local_name != "rss" {
+            return Err(Error::from(InvalidRssError(format!(
+                "invalid root element name: {}",
+                name.local_name
+            ))));
+        }
+        let version = attrs
+            .iter()
+            .find(|a| a.name.to_string() == "version")
+            .map(|a| a.value.as_ref());
+        match version {
+            Some("2.0") => Ok(()),
+            Some(version) => Err(Error::from(InvalidRssError(format!(
+                "unsupported rss version: {}",
+                version
+            )))),
+            None => Err(Error::from(InvalidRssError(
+                "missing version attribute".to_string(),
+            ))),
+        }
+    }
+    fn get_results(&self) -> Vec<Rss> {
+        self.results.clone()
     }
 }
 
